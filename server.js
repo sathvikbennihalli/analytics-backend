2a92790bddfb1ea7464c1beb7dce5a53cf9868c9
“sathvikbennihalli” <sathwikrb21@gmail.com>
2024-05-27 16:43:56 +0530

    boiler plate code

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
