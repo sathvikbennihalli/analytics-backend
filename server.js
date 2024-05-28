66f8e221a02ea2ffd3dff69fc7aa39fad61ec7d7
“sathvikbennihalli” <sathwikrb21@gmail.com>
2024-05-28 11:31:46 +0530

    updated css files

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-const path = require("path");
+import { dirname, join } from "path";
+import express from "express";
+import { fileURLToPath } from "url";
 
-const express = require("express");
+import rootRouter from "./routes/api/root.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello!!");
+app.use("/", express.static(join(__dirname, "public")));
+
+app.use("/", rootRouter);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.listen(PORT, () => {
